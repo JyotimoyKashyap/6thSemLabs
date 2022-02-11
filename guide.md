21000555ffb5f23d3af77c89db71a8aa6d0efc9d
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2022-02-11 16:05:23 +0530

    added lab 1 files

---
+++ guide.md
@@ -0,0 +1,12 @@
+# Guide to OpenGL
+
+## Compilation of C Code 
+Use the code below to compile the .c file with the Glut libraries 
+
+`g++ main.c -lglut -lGL -lGLEW -lGLU -o <filename>`
+
+## Running the file 
+
+To run the file, simple run in the terminal. 
+
+`./<filename>`
---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glColor3f(1, 0, 0);
+        glVertex2f(0.0, 0.0);
+        glVertex2f(0.5, 0.0);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.0, 0.5);
+    glEnd();
+
+    glFlush();
+}
+
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(640, 640);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("OpenGL Lab");
+    glClearColor(1, 1, 1, 1);
+    glutDisplayFunc(displayMe);
+    glLoadIdentity();
+    glutMainLoop();
+
+    return 0;
+}
